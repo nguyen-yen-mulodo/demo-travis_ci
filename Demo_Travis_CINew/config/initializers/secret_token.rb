d23b2ba1147ac74bc96c77ae3a7295c91cd454ee
haiyen <nguyen.yen@mulodo.com>
2013-09-09 14:11:25 +0700

    ghh

---
+++ Demo_Travis_CINew/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoTravisCinew::Application.config.secret_key_base = '34b3ae88d82168c1b166437ba55e79a3e2d37d46c9750716bd21e4fca48a50aab031ddb09282b9110773c0acf458a26f9feb06a1bc7e4b6d305ba0f1344ce68b'
---
+++ Demo_Travis_CINew/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DemoTravisCinew::Application.config.session_store :cookie_store, key: '_Demo_Travis_CINew_session'
